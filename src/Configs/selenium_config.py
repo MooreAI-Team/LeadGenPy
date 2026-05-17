350d8ca736d4b99f10b7e2def1627daeea5483c1
laween-alsulaivany <laweenhamza@gmail.com>
2026-05-17 17:51:49 -0500

    refactor selenium_config.py to remove manual mode selection and streamline headless/browser mode handling

--- src/Configs/selenium_config.py
+++ src/Configs/selenium_config.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from Configs import config
+
+
 def __initialize():
     chrome_options = webdriver.ChromeOptions()
-    mode = int(input("<== SELENIUM: ==>\n1 - Launch headless Mode\n2 - Development Mode (Browswer Mode)\nEnter the value : "))
-    if mode == 1:
-        chrome_options.add_argument("--headless=new")
-    
+
+    # block cookies, images, popups, notifs etc to speeds things up
     prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2,
                                                         'plugins': 2, 'popups': 2, 'geolocation': 2,
                                                         'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2,
@@ -14,13 +15,22 @@ def __initialize():
                                                         'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2,
                                                         'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2,
                                                         'durable_storage': 2}}
-    chrome_options.add_argument("start-maximized")
     chrome_options.add_experimental_option('prefs', prefs)
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")  # no extensions
+    chrome_options.add_argument("--disable-gpu")  # needed on some windows setups
+    chrome_options.add_argument("--disable-infobars")  # hide "chrome is being controlled" bar
 
-    print("[LOG] - INITIALIZED SELENIUM DRIVER")
-    return webdriver.Chrome(
-    '../assets/chromedriver.exe', chrome_options=chrome_options)
+    if config.DEV:
+        # dev: open browser so we can see what's happening
+        print("[LOG] Dev mode: browser visible")
+        chrome_options.add_argument("start-maximized")
+    else:
+        # prod: no UI needed
+        print("[LOG] Prod mode: headless")
+        chrome_options.add_argument("--headless=new")
+
+    print("[LOG] Selenium ready")
+    return webdriver.Chrome(options=chrome_options)
+
+
 driver = __initialize()
